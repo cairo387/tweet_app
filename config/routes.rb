7a7405284923ee29cdef64dc29bcecd6ee6e70d1
cairo387 <mifuneri@outlook.jp>
2021-05-12 19:10:34 +0000

    [Add]詳細ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts/index'
   root to: 'home#top'
   get 'about' => 'home#about'
+
+  resources :posts, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
