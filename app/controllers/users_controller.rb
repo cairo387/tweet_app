7cf57fd2d0041a860fe75763d4f5f2bca01110a2
cairo387 <mifuneri@outlook.jp>
2021-05-13 18:08:00 +0000

    [Add]ログインページのエラーメッセージの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,9 +38,20 @@ class UsersController < ApplicationController
   def login_form
   end
 
+  def login
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      flash[:notice] = "ログインに成功しました！"
+      redirect_to user_path(@user)
+    else
+      @error_message = "入力したメールアドレス、パスワードのいずれかが間違っています。"
+      render :login_form
+    end
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :image)
+    params.require(:user).permit(:name, :email, :image, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#top'
   get 'about' => 'home#about'
   get 'login_user' => 'users#login_form'
+  post 'login_user' => 'users#login'
 
   resources :posts
   resources :users, only: [:index, :show, :edit, :update, :new, :create]
