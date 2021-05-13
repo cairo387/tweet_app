d0c629e7f382376b28e47e534f695a977b0f4535
cairo387 <mifuneri@outlook.jp>
2021-05-13 18:58:46 +0000

    [Add]ログイン状態かどうか判別するアクション追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+
+  def set_current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
+#ログイン状態かどうか判別する
+  def authenticate_user
+    if @current_user.nil?
+      flash[:notice] = "ログインが必要です"
+      redirect_to login_user_path
+    end
+  end
 end
