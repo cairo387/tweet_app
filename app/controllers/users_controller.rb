bd9d911666740aa5ca664cd7e6dbb96482074fb1
cairo387 <mifuneri@outlook.jp>
2021-05-13 17:12:19 +0000

    [Add]ユーザー詳細作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,35 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "ユーザー情報の編集をしました"
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "ユーザーの新規作成をしました"
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   private
