822bbf95bde9fdd4209293cbc4f246944ccf161d
cairo387 <mifuneri@outlook.jp>
2021-05-13 07:14:49 +0000

    [Add]エラーメッセージの表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.new(post_params)
-    if post.save
+    @post = Post.new(post_params)
+    if @post.save
       redirect_to posts_path
     else
       render :new
@@ -25,9 +25,9 @@ class PostsController < ApplicationController
   end
 
   def update
-    post = Post.find(params[:id])
-    if post.update(post_params)
-      redirect_to post_path(post)
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post)
     else
       render :edit
     end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  #バリデーション
+  validates :content, {presence:true, length:{maximum: 140}}
 end
