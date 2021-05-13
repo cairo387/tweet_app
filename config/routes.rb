09558bd24aa9c74627d23d99fb710cf2e0b79b93
cairo387 <mifuneri@outlook.jp>
2021-05-13 10:32:18 +0000

    [Add]ユーザー一覧画面等

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/new'
   root to: 'home#top'
   get 'about' => 'home#about'
 
   resources :posts
+  resources :users, only: [:index, :show, :edit, :update, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
