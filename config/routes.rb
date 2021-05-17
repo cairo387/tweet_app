eddac9dc9639bf3253a18d7f6ea1c4f86e9ae06a
cairo387 <mifuneri@outlook.jp>
2021-05-17 10:12:30 +0000

    [Add]Likesコントローラー、ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   post 'login_user' => 'users#login'
   post 'logout_user' => 'users#logout'
 
-  resources :posts
+  resources :posts do
+    resource :like ,only: [:create, :destroy]
+  end
   resources :users, only: [:index, :show, :edit, :update, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
