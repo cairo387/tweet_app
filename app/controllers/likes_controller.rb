75e99bd5ce31a443eaf2c50ca6146c5c1c4fb45e
cairo387 <mifuneri@outlook.jp>
2021-05-17 10:23:06 +0000

    [Add]いいね表示（仮）追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,6 +2,12 @@ class LikesController < ApplicationController
   before_action :authenticate_user
   
   def create
+    @like = Like.new
+    @like.user_id = @current_user.id
+    @like.post_id = params[:post_id]
+    if @like.save
+      redirect_to post_path(params[:post_id])
+    end
   end
   
   def destroy
